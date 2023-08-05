8aaa19b3b6840446b2d7c312efc49724178cbf01
Falk Ahlendorf <falk.ahlendorf@googlemail.com>
2023-08-05 17:15:53 +0200

    Load and use the custom font

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+
+#include "ec1834.hpp"
+
 TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
@@ -8,15 +11,29 @@ void setup() {
   Serial.println("Hello World!");
   
   tft.init();
+  tft.setAttribute(UTF8_SWITCH,0);
   tft.setRotation(1);
-  tft.fillScreen(TFT_BLUE);
-  tft.setTextFont(1);
+  tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.println("Hello World!");
+  ec1834bin_select(tft);
+  
+  tft.setCursor(2,2);
+  tft.printf("\260\261\262\333           \333\262\261\260");
+  tft.setTextColor(TFT_BLACK,TFT_GREEN);
+  tft.drawString(" Hello 000 ",4*8+2,2);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.printf("");
+  tft.setTextColor(TFT_BROWN, TFT_BLACK);
+  tft.printf("\337\337\337\337\337\337\337\337\337\337\337\337\337\337\337\337\337\337\337\337");
+  tft.setTextColor(TFT_LIGHTGREY, TFT_BLACK);
+  tft.printf("This is using the original EC1834 font ROM");  
 }
-
+unsigned count = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello World!");
-  delay(1000);
+  tft.setTextColor(TFT_BLACK,TFT_GREEN);
+  tft.drawString("   ",11*8+2,2);
+  tft.drawNumber(count%1000,11*8+2,2);
+  ++count;
+  delay(500);
 }
