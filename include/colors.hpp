f6f69f3f6aa25a36101f895879aefcccadabc829
Falk Ahlendorf <falk.ahlendorf@googlemail.com>
2023-08-06 16:27:10 +0200

    Added color header

---
+++ include/colors.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <TFT_eSPI.h>
+
+namespace UI
+{
+    typedef uint32_t Color;
+    
+    enum {
+        DefaultFaceColor = TFT_BLACK,
+        DefaultTextColor = TFT_GREEN 
+    };
+}
